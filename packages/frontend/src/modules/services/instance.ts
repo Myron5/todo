31cfed96ea8c951e804193fe31f95d4221b9d4eb
ivan1melnyk <melnyk.ivan20a@gmail.com>
2024-06-05 16:23:24 +0200

    change baseURL Back

--- packages/frontend/src/modules/services/instance.ts
+++ packages/frontend/src/modules/services/instance.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: 'http://localhost:4200'
+  // baseURL: 'http://localhost:4200',
+  baseURL: 'https://todo-j75o.onrender.com'
 });
 
 export const setToken = (token: string) => {
